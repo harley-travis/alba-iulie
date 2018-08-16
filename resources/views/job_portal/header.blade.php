3ecf9a13bc7b2d3ad558bdcbed8635acd23fc39b
harley-travis <harley.travis2@gmail.com>
2018-08-16 12:01:25 -0600

    displaying data in job_portal and moduarlized

---
+++ resources/views/job_portal/header.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
+
+        <title>Job Page</title>
+
+        <link href="{{ asset('css/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+
+    </head>
+    <body>
--- resources/views/job_portal/job.blade.php
+++ resources/views/job_portal/job.blade.php
@@ -1,18 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
-
-        <title>Job Page</title>
-
-        <link href="{{ asset('css/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-
-    </head>
-    <body>
-        @foreach($job as $job)
+@include('job_portal.header')
+@foreach($job as $job)
             <div class="container-fluid">
                 <div class="container">
                     <div class="left-container">
@@ -21,21 +8,57 @@
                             <h3>{{$job->company_name}}</h3>
                             <h4>{{$job->location}}</h4>
                         </header>
+                        <section class="job-section">
+                            <h2 class="section-title">Description</h2>
+                            <p>{{$job->description}}</p>
+                        </section>
+                        <section class="job-section">
+                            <h2 class="section-title">What You'll Do</h2>
+                            <p>{{$job->work}}</p>
+                        </section>
+                        <section class="job-section">
+                            <h2 class="section-title">Qualifications</h2>
+                            <p>{{$job->qualifications}}</p>
+                        </section>
+                        <section class="job-section">
+                            <h2 class="section-title">Preferred Skills</h2>
+                            <p>{{$job->skills}}</p>
+                        </section>
+                        <section class="job-section">
+                            <h2 class="section-title">About {{$job->company_name}}</h2>
+                            <p>{{$job->bio}}</p>
+                        </section>
+                        <div class="bottom-cta">
+                            <a href="#" class="btn btn-secondary">See More Jobs</a>
+                            <a href="#" class="btn btn-success">Apply</a>
+                        </div>
                     </div><!-- left-container -->
                     <div class="right-container">
                         <div class="company-logo-container">
                             <div class="company-logo-wrapper">
-                                <img src="#" alt="#" class="company-logo">
+                                <img src="{{ '/images/logos/avengers-logo.png' }}" alt="#" class="company-logo">
                             </div>
                         </div>
                         <div class="job-data-container">
                             <div class="job-data-wrapper">
-
+                                <div class="sidebar-cta">
+                                    <a href="#" class="btn btn-secondary">See More Jobs</a>
+                                    <a href="#" class="btn btn-success">Apply</a>
+                                </div>
+                                <div class="job-info">
+                                    <ul>
+                                        <li>{{$job->compensationAmount}}</li>
+                                        <li>{{$job->duration}}</li>
+                                        <li>Close Date {{$job->closeDate}}</li>
+                                    </ul>
+                                </div>
+                                <div class="wj-tag">
+                                    <span>Powered by <span class="wj-minor">White July</span></span>
+                                </div>
                             </div>
                         </div>
                     </div><!-- right-container -->
                 </div>
             </div>
-        @endforeach
-    </body>
-</html>
+            @endforeach
+@include('job_portal.footer')
